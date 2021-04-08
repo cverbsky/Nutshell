48772281b852d8dbf976b7d7e51ce45719288137
Christa Verbsky <61993357+cverbsky@users.noreply.github.com>
2021-04-07 23:36:40 -0400

    Add files via upload
    VERY ROUGH outline of main.c

---
+++ main.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void shell_init();
+void printPrompt();
+int getCommand();
+void handle_errors();
+void processCommand();
+
+int main()
+{
+    shell_init();
+
+    while(1)
+    {
+        printPrompt();
+        // command = -1 for ERROR, 0 for BYE and 1 for OK
+        int command = getCommand();
+        if (command == -1)
+            handle_errors();
+        else if (command == 1)
+            processCommand();
+        else if (command == 0)
+            exit(0);
+    }
+}
+
+void shell_init()
+{
+    /* TAKEN FROM LECTURE 23 POWERPOINT */
+    // init all variables.
+    // define (allocate storage) for some var/tables
+    // init all tables (e.g., alias table, command table)
+    // get PATH environment variable (use getenv())
+    // get HOME env variable (also use getenv())
+    // disable anything that can kill your shell.
+    // (the shell should never die; only can be exit)
+    // do anything you feel should be done as init
+
+
+}
+void printPrompt()
+{
+    printf("$ or something");
+}
+int getCommand()
+{
+    /*
+    init_scanner-and_parser();
+    if (yyparse() != 0)
+        understand_errors();
+    else
+        return 1;
+        */
+
+}
+void handle_errors()
+{
+    /* TAKEN FROM LECTURE 23 POWERPOINT */
+    // Find out if error occurred in middle of command,
+    // that is, the command still has a “tail”
+    // In this case you have to recover by “eating”
+    // the rest of the command.
+    // To do this: you may want to use yylex() directly, or
+    // handle clear things up in any other way.
+
+}
+void processCommand()
+{
+    // if builtin do one thing
+    //otherwise execute command
+}
