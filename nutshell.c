77e18cefecd5b0816e7624389bea1100d5d484e9
Christa Verbsky <61993357+cverbsky@users.noreply.github.com>
2021-04-15 12:22:45 -0400

    Update nutshell.c

--- nutshell.c
+++ nutshell.c
@@ -1,22 +1,48 @@
-// This is ONLY a demo micro-shell whose purpose is to illustrate the need for and how to handle nested alias substitutions and how to use Flex start conditions.
-// This is to help students learn these specific capabilities, the code is by far not a complete nutshell by any means.
-// Only "alias name word", "cd word", and "bye" run.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "global.h"
 #include <unistd.h>
+#include<fcntl.h>
 
+void shell_init();
+void printPrompt();
+int getCommand();
+void handle_errors();
+void processCommand();
 char *getcwd(char *buf, size_t size);
+
 int yyparse();
 
 int main()
 {
-	if(!isatty(0))
-	{
-		//read in piped input here
-		printf("reading from a pipe\n");
-	}
+    shell_init();
+
+    while(1)
+    {
+        printPrompt();
+        // command = -1 for ERROR, 0 for BYE and 1 for OK
+        int command = getCommand();
+        if (command == -1)
+            handle_errors();
+        else if (command == 1)
+            processCommand();
+        else if (command == 0)
+            exit(0); // redundant, exit likely handled in parser
+    }
+}
+
+void shell_init()
+{
+    /* TAKEN FROM LECTURE 23 POWERPOINT */
+    // init all variables.
+    // define (allocate storage) for some var/tables
+    // init all tables (e.g., alias table, command table)
+    // get PATH environment variable (use getenv())
+    // get HOME env variable (also use getenv())
+    // disable anything that can kill your shell.
+    // (the shell should never die; only can be exit)
+    // do anything you feel should be done as init
 
     aliasIndex = 0;
     varIndex = 0;
@@ -36,12 +62,42 @@ int main()
     strcpy(varTable.word[varIndex], ".:/bin");
     varIndex++;
 
-    system("clear");
-    while(1)
-    {
-        printf("nutshell@%s>> ", varTable.word[0]);
-        yyparse();
-    }
+    // initialize command table
+    commandTable.argCount = 0;
 
-   return 0;
+    inputFile = false;
+    outputFile = false;
+    inBackground = false;
+
+    system("clear");
+}
+void printPrompt()
+{
+    printf("[%s]>> ", varTable.word[2]);;
+}
+int getCommand()
+{
+    // init_scanner-and_parser();
+    int yyParseReturn = yyparse();
+    if (yyParseReturn == 1)
+        return 1;
+    else
+        printf("Parsing failed");
+    return -1;
+}
+void handle_errors()
+{
+    /* TAKEN FROM LECTURE 23 POWERPOINT */
+    // Find out if error occurred in middle of command,
+    // that is, the command still has a “tail”
+    // In this case you have to recover by “eating”
+    // the rest of the command.
+    // To do this: you may want to use yylex() directly, or
+    // handle clear things up in any other way.
+
+}
+void processCommand()
+{
+    // builtins handled within parser
+
 }
